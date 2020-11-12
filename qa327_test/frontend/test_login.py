64708b9feb41927fc93c23122c5d44d3d920d012
Eric Fillion <ericfillioncanada@gmail.com>
2020-11-12 16:18:52 -0500

    removed unused imports

--- qa327_test/frontend/test_login.py
+++ qa327_test/frontend/test_login.py
@@ -3,9 +3,8 @@ import pytest
 from seleniumbase import BaseCase
 
 from qa327_test.conftest import base_url
-from unittest.mock import patch
 from qa327.models import db, User
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash
 
 """
 This file defines all unit tests for the login page
