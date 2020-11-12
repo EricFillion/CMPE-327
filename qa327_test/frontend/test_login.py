72c267e707e78fa2092e2510fff90f91e1191091
Eric Fillion <ericfillioncanada@gmail.com>
2020-11-12 16:17:01 -0500

    Added login test case

---
+++ qa327_test/frontend/test_login.py
@@ -0,0 +1,34 @@
+
+import pytest
+from seleniumbase import BaseCase
+
+from qa327_test.conftest import base_url
+from unittest.mock import patch
+from qa327.models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+"""
+This file defines all unit tests for the login page
+"""
+
+# Moch a sample user
+test_user = User(
+    email='testFrontend@test.com',
+    name='testFrontend',
+    password=generate_password_hash('testFrontend'),
+    balance=200
+)
+
+class FrontEndLoginPageTest(BaseCase):
+    """
+    A class that contains the unit tests for the login page
+    """
+
+    def test_login_success(self, *_):
+        """
+         R1.1: if the user hasn’t logged in, show the login page
+        """
+        # open login page
+        self.open(base_url + '/logout')
+
+        self.assert_element("#log-in")
