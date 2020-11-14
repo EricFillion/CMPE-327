7c2fb66c3105996f89de696c49a4e31717c3b59f
Cooper Harasyn <charasyn@outlook.com>
2020-11-14 13:49:58 -0500

    👓👕 Lint and clean up some comments

--- qa327_test/common.py
+++ qa327_test/common.py
@@ -1,40 +1,49 @@
+"""
+qa327_test/common.py:
+A shared space for any common testing code/data.
+"""
 from unittest.mock import patch
-
-from qa327_test.conftest import base_url
-from qa327.models import db, User
 from werkzeug.security import generate_password_hash
 
+from qa327_test.conftest import base_url
+from qa327.models import User
+
 # Mock a sample user
-test_user = User(
+TEST_USER = User(
     email='test_frontend@example.com',
     name='Test Frontend',
     balance=2000
 )
-test_user.raw_password = 'q1w2e3Q!W@E#'
-test_user.password = generate_password_hash(test_user.raw_password)
+TEST_USER.raw_password = 'q1w2e3Q!W@E#'
+TEST_USER.password = generate_password_hash(TEST_USER.raw_password)
 
 def auto_login(user):
-    def generate_wrapper(fn):
+    """
+    Function decorator which will automatically log-in a given user.
+    This includes patching the backend functions so that they will
+    always use this specific user.
+    """
+    def generate_wrapper(func):
         def wrapped(self, *args, **kwargs):
-            # - Open `/logout` (to invalidate any previous session)
+            # Open `/logout` (to invalidate any previous session)
             self.open(base_url + '/logout')
-            # - Open `/login`
+            # Open `/login`
             self.open(base_url + '/login')
-            # - Enter test_user's email into element `#email`
+            # Enter given user's email into element `#email`
             self.type("#email", user.email)
-            # - Enter test_user's password into element `#password`
+            # Enter given user's password into element `#password`
             self.type("#password", user.raw_password)
-            # - Click element `input[type='submit']`
+            # Click element `input[type='submit']`
             self.click('input[type="submit"]')
 
             # Call user testing function, preserving any return value
-            return_value = fn(self, *args, **kwargs)
+            return_value = func(self, *args, **kwargs)
 
-            # - Open `/logout` (cleanup)
+            # Open `/logout` (cleanup)
             self.open(base_url + '/logout')
 
             return return_value
-        p1 = patch('qa327.backend.get_user', return_value=user)(wrapped)
-        p2 = patch('qa327.backend.login_user', return_value=user)(p1)
-        return p2
+        patched_1 = patch('qa327.backend.get_user', return_value=user)(wrapped)
+        patched_2 = patch('qa327.backend.login_user', return_value=user)(patched_1)
+        return patched_2
     return generate_wrapper
--- qa327_test/frontend/test_auto_login.py
+++ qa327_test/frontend/test_auto_login.py
@@ -1,9 +1,21 @@
+"""
+qa327_test/frontend/test_auto_login.py:
+A single test verifying the correct operation of the common auto_login function.
+"""
 from seleniumbase import BaseCase
 from qa327_test.conftest import base_url
-from qa327_test.common import auto_login, test_user
+from qa327_test.common import auto_login, TEST_USER
 class FrontEndAutoLoginTest(BaseCase):
-    @auto_login(test_user)
+    """
+    Class containing test cases for the auto_login function.
+    """
+    @auto_login(TEST_USER)
     def test_auto_login(self, *_):
+        """
+        Test the auto_login function decorator.
+        This doesn't follow any specific requirement, although it may duplicate
+        some of the login testing.
+        """
         self.open(base_url + '/')
         # Wait up to 3 seconds for page to be ready
         self.wait_for_ready_state_complete(timeout=3)
