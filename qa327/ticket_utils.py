5e999ffe6a3a6f08a95b72de1bb8b9e5f1831c94
TingzhouJia <joe.tzjia@gmail.com>
2020-12-01 19:55:47 -0500

    :hammer: add ticket utils

--- qa327/ticket_utils.py
+++ qa327/ticket_utils.py
@@ -1,18 +1,8 @@
-import re
-
-def validate_ticket_name_format(ticket_name):
-    """
-    
-    """
-    if not re.match("^[a-zA-Z0-9][a-zA-Z0-9_ ]+[a-zA-Z0-9]$", ticket_name):
-        return False
-    elif len(ticket_name)>60:
-        return False
-    return True
-
-def validate_ticket_quantity(quantity):
-    return quantity>0 and quantity<100
-
 
 def calculate_price_ticket(quantity,price):
+    '''
+    calculate price of quantity
+    :param quantity:  number of ticket to buy
+    :param price: price of each ticket
+    '''
     return 1.4 * (quantity * price)
