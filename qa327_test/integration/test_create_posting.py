edd2bdf14ef1877981f56ec7b4846778451df09d
Cooper Harasyn <charasyn@outlook.com>
2020-12-08 21:19:57 -0500

    ✅💚 Implementation of integration tests I1, I2
    This commit implements integration tests I1 and I2.
    These follow the user flow through the following steps:
    - registration
    - login
    - posting/purchasing ticket (I1/I2)
    - logout

---
+++ qa327_test/integration/test_create_posting.py
@@ -0,0 +1,88 @@
+"""
+This file defines integration test I1
+"""
+
+from unittest.mock import patch
+from seleniumbase import BaseCase
+
+from qa327_test.conftest import base_url
+from qa327_test.common import TEST_USER, TEST_TICKET
+from qa327.models import Ticket
+from datetime import date, timedelta
+
+class IntegrationCreatePostingTest(BaseCase):
+    """
+    A class that contains the integration test which posts a new ticket
+    for sale in the app
+    """
+
+    def test_integration_create_posting(self):
+        """
+        This testcase follows a complete userflow through ticket posting
+        """
+        # ### Overview of major steps
+        # 1. User registers
+        # 2. User logs in
+        # 3. User sells ticket
+        # 4. User logs out
+        # ### Notes
+        # - The test_user and test_ticket defined in `qa327_test/common.py` will be used throughout this test
+        # ### Initialization
+        # - Clear all contents from database (done by pytest fixture)
+        # - Open `/logout` to ensure there is no previous session
+        self.open(base_url + '/logout')
+        # ### 1. User registers (R2.10)
+        # - Open `/`
+        self.open(base_url + '/')
+        # - Validate redirection to `/login`
+        self.assert_equal(self.get_current_url(), base_url + '/login')
+        # - Click "Register" link (element `a[href="/register"]`)
+        self.click('a[href="/register"]')
+        # - Enter test_user's name into element `#name`
+        self.type("#name", TEST_USER.name)
+        # - Enter test_user's email into element `#email`
+        self.type("#email", TEST_USER.email)
+        # - Enter test_user's password into element `#password`
+        self.type("#password", TEST_USER.raw_password)
+        # - Enter test_user's password into element `#password2`
+        self.type("#password2", TEST_USER.raw_password)
+        # - Click "Register" button (element `input[type="submit"]`)
+        self.click('input[type="submit"]')
+        # - Validate redirection to `/login`
+        self.assert_equal(self.get_current_url(), base_url + '/login')
+        # ### 2. User logs in (R1.9)
+        # - Enter test_user's email into element `#email`
+        self.type("#email", TEST_USER.email)
+        # - Enter test_user's password into element `#password`
+        self.type("#password", TEST_USER.raw_password)
+        # - Click "Login" button (element `input[type="submit"]`)
+        self.click('input[type="submit"]')
+        # - Validate redirection to `/`
+        self.assert_equal(self.get_current_url(), base_url + '/')
+        # ### 3. User sells ticket (R4.7 + R3.5.1 for validation)
+        # - Enter test_ticket’s name in the element `#sellform_input_name`
+        self.type("#sellform_input_name", TEST_TICKET.name)
+        # - Enter test_ticket’s quantity into the element `#sellform_input_quantity`
+        self.type("#sellform_input_quantity", TEST_TICKET.quantity)
+        # - Enter test_ticket’s raw expiry date into the element `#sellform_input_date`
+        self.type("#sellform_input_expiry", TEST_TICKET.raw_expiry)
+        # - Enter test_ticket’s price into the element `#sellform_input_price`
+        self.type("#sellform_input_price", '{:.2f}'.format(TEST_TICKET.price/100))
+        # - Click element `#sellform_submit`
+        self.click('#sellform_submit')
+        # - Validate that an element matching `.message_info` shows text `Ticket was posted for sale successfully.`
+        self.assert_text('Ticket was posted for sale successfully.', selector='.message_info')
+        # - Find `tr` of ticket by looking up ticket's name under the ticket table
+        rows = self.find_elements("table#tickettable tbody tr")
+        matching_trs = [row for row in rows if TEST_TICKET.name in row.text]
+        self.assert_equal(len(matching_trs), 1)
+        tr = matching_trs[0]
+        # - Validate name by looking under the ticket's `tr` and validating that the element `td.tt_name` has text: `"{}".format(ticket.name)`
+        self.assert_equal(tr.find_element_by_class_name("tt_name").text, "{}".format(TEST_TICKET.name))
+        # - Validate owner's email by looking under the ticket's `tr` and validating that the element `td.tt_owner` has text: `"{}".format(user.name)`
+        self.assert_equal(tr.find_element_by_class_name("tt_owner").text, "{}".format(TEST_USER.name))
+        # ### 4. User logs out (R7.1)
+        # - Click element `#logout`
+        self.open(base_url + '/logout')
+        # - Validate redirection to `/login`
+        self.assert_equal(self.get_current_url(), base_url + '/login')
---
+++ qa327_test/integration/test_purchase_ticket.py
@@ -0,0 +1,112 @@
+"""
+This file defines integration test I1
+"""
+
+from unittest.mock import patch
+from seleniumbase import BaseCase
+
+from qa327_test.conftest import base_url
+from qa327_test.common import TEST_USER, TEST_TICKET
+from qa327.models import User, Ticket, db
+from datetime import date, timedelta
+
+class IntegrationPurchaseTicketTest(BaseCase):
+    """
+    A class that contains the integration test which purchases a ticket
+    through the app
+    """
+
+    def test_integration_purchase_ticket(self):
+        """
+        This testcase follows a complete userflow through ticket purchase
+        """
+        # ### Overview of major steps
+        # 1. User registers
+        # 2. User logs in
+        # 3. User buys ticket
+        # 4. User logs out
+        # ### Notes
+        # - The test_user defined in `qa327_test/common.py` will be used throughout this test
+        # - An additional test user `test_seller` will be manually added to the database to serve as the user who posted the ticket
+        #     - Email: "test_seller@test.com"
+        #     - Name: "Test Seller"
+        #     - Password: None (we will not login as this user, so it is not necessary to set their password)
+        #     - Balance: 0 (they will not need to spend any balance)
+        TEST_SELLER = User(
+            email="test_seller@test.com",
+            name="Test Seller",
+            password="",
+            balance=0
+        )
+        # - An additional test ticket "test_selling_ticket" will be manually added to the database
+        #     - It will be based on the test_ticket provided in common.py, but with the following information changed:
+        #     - Owner: `test_seller`
+        #     - Owner ID: [corresponding value for test_seller, set automatically from code]
+        TEST_SELLING_TICKET = Ticket(
+            name=TEST_TICKET.name,
+            quantity=TEST_TICKET.quantity,
+            price=TEST_TICKET.price,
+            expiry=TEST_TICKET.expiry,
+            owner=TEST_SELLER
+        )
+        # ### Initialization
+        # - Clear all contents from database (done by pytest fixture)
+        # - Add "test_seller" and "test_selling_ticket" to database
+        db.session.add(TEST_SELLER)
+        db.session.add(TEST_SELLING_TICKET)
+        db.session.commit()
+        # - Open `/logout` to ensure there is no previous session
+        self.open(base_url + '/logout')
+        # ### 1. User registers (R2.10)
+        # - Open `/`
+        self.open(base_url + '/')
+        # - Validate redirection to `/login`
+        self.assert_equal(self.get_current_url(), base_url + '/login')
+        # - Click "Register" link (element `a[href="/register"]`)
+        self.click('a[href="/register"]')
+        # - Enter test_user's name into element `#name`
+        self.type("#name", TEST_USER.name)
+        # - Enter test_user's email into element `#email`
+        self.type("#email", TEST_USER.email)
+        # - Enter test_user's password into element `#password`
+        self.type("#password", TEST_USER.raw_password)
+        # - Enter test_user's password into element `#password2`
+        self.type("#password2", TEST_USER.raw_password)
+        # - Click "Register" button (element `input[type="submit"]`)
+        self.click('input[type="submit"]')
+        # - Validate redirection to `/login`
+        self.assert_equal(self.get_current_url(), base_url + '/login')
+        # ### 2. User logs in (R1.9 + R3.3 for balance validation)
+        # - Enter test_user's email into element `#email`
+        self.type("#email", TEST_USER.email)
+        # - Enter test_user's password into element `#password`
+        self.type("#password", TEST_USER.raw_password)
+        # - Click "Login" button (element `input[type="submit"]`)
+        self.click('input[type="submit"]')
+        # - Validate redirection to `/`
+        self.assert_equal(self.get_current_url(), base_url + '/')
+        # - Validate that an element `#balance` shows text `Your balance is $50.00`
+        self.assert_text('Your balance is $50.00', selector='#balance')
+        # ### 3. User buys ticket (R6.5.1 + R3.5.1 for quantity validation + R3.3 for balance validation)
+        # - Enter test_ticket’s name in the element `#buyform_input_name`
+        self.type("#buyform_input_name", TEST_SELLING_TICKET.name)
+        # - Enter a quantity of 2 into the element `#buyform_input_quantity`
+        self.type("#buyform_input_quantity", 2)
+        # - Click element `#buyform_submit`
+        self.click('#buyform_submit')
+        # - Validate that an element `.message_info` shows text `Ticket was purchased successfully.`
+        self.assert_text('Ticket was purchased successfully.', selector='.message_info')
+        # - Find `tr` of ticket by looking up ticket's name under the ticket table
+        rows = self.find_elements("table#tickettable tbody tr")
+        matching_trs = [row for row in rows if TEST_TICKET.name in row.text]
+        self.assert_equal(len(matching_trs), 1)
+        tr = matching_trs[0]
+        # - Validate quantity by looking under the ticket's `tr` and validating that the element `td.tt_quantity` has text: `88`
+        self.assert_equal(tr.find_element_by_class_name("tt_quantity").text, "{}".format(88))
+        # - Validate that an element `#balance` shows text `Your balance is $8.00`
+        self.assert_text('Your balance is $8.00', selector='#balance')
+        # ### 4. User logs out (R7.1)
+        # - Click element `#logout`
+        self.open(base_url + '/logout')
+        # - Validate redirection to `/login`
+        self.assert_equal(self.get_current_url(), base_url + '/login')
