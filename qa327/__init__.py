6c164705a5e03cd345439dc8c1a59c473ded7fb2
Jolene Jane Lammers <jjl10@queensu.ca>
2020-12-12 11:51:54 -0500

    :bug: Fix error in __init__.py from adding docker changes

--- qa327/__init__.py
+++ qa327/__init__.py
@@ -17,18 +17,25 @@ templates = os.path.join(
 app = Flask('this is a simple web application', template_folder=templates)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '69cae04b04756f65eabcd2c5a11c8c24'
-# if the user supplies a database file name, we use
-# that instead, and it should an absolute path
-# for windows user, C:\ is the root directory, so it
-# should not be included. e.g. Users/xxx/Document/db.sqlite
-db_name = os.getenv('DB_NAME')
+# Previously:
+# # if the user supplies a database file name, we use
+# # that instead, and it should an absolute path
+# # for windows user, C:\ is the root directory, so it
+# # should not be included. e.g. Users/xxx/Document/db.sqlite
+# We no longer do this, since set_db_path will be called with
+# a temp dir from tempfile.TemporaryDirectory(), which WILL
+# have the drive letter specifier on it. In other words, this
+# should always take an absolute path, including drive specifier
+# on Windows.
+db_name_from_env = os.getenv('DB_NAME')
 db_string = os.getenv('db_string')
-if db_name:
-    database_url = "sqlite:////" + db_name
-elif db_string:
-    database_url = db_string
-else:
-    # default:
-    # db.sqlite at the working directory
-    database_url = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+def set_db_path(db_path: str):
+    if db_path:
+        database_url = "sqlite:///" + db_path
+    elif db_string:
+        database_url = db_string
+    else:
+        # Default to using 'db.sqlite' file in current directory.
+        database_url = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+set_db_path(db_name_from_env)
